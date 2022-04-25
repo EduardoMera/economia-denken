da68df6bbd84f77caadd14ecdd1440d456c828b2
FrancoDiMartino <francogdimartino@gmail.com>
2022-04-25 08:11:04 -0300

    Pre Corte de Luz

--- componentes/navbar.js
+++ componentes/navbar.js
@@ -4,8 +4,14 @@ window.addEventListener('DOMContentLoaded', (e)=>{
     if(ancho<=576){
         let barraarriba=document.getElementById('barra-arriba')
         document.getElementById('barra-arriba-mobile').innerHTML=barraarriba.innerHTML
-        barraarriba.innerHTML=''
+        barraarriba.remove()
 
         document.getElementById('contenedor-navbar').classList.remove('container-fluid')
+
+
+        let buscador=document.querySelector('#buscador')
+        document.getElementById('buscador-mobile').appendChild(buscador).
+        buscador.remove()
+
     }
 });
