3bda1210fb4463cde68878a2d6b8bda9ba13e39d
Juan Chapur <juanchapur99@gmail.com>
2022-05-08 03:00:18 -0300

    commit respaldo

--- componentes/navbar.js
+++ componentes/navbar.js
@@ -8,10 +8,21 @@ window.addEventListener('DOMContentLoaded', (e)=>{
 
         document.getElementById('contenedor-navbar').classList.remove('container-fluid')
 
-
         let buscador=document.querySelector('#buscador')
-        document.getElementById('buscador-mobile').appendChild(buscador).
-        buscador.remove()
+        document.getElementById('buscador-mobile').appendChild(buscador);
+        document.getElementById("buscador").remove();
 
+        adaptarNavbar();
     }
 });
+
+function adaptarNavbar(){
+    let haySubnivelesDisponibles = true;
+    let nivel = 0;
+    const navbar = []
+    while(haySubnivelesDisponibles){
+        const palabrasClave = [...document.querySelectorAll(`.level-${nivel}`).values()].map(item => item.textContent);
+        console.log(palabrasClave);
+        haySubnivelesDisponibles = false
+    }
+}
